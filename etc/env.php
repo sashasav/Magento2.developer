e1460df20f4063565d131c6609e9e963c4735004
Oleksandr Savchuk <sashasavch@gmail.com>
2019-09-29 21:37:56 +0300

    added app/code/Smile/Contact/etc folder

---
+++ etc/env.php
@@ -0,0 +1,69 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_39f828'
+    ],
+    'crypt' => [
+        'key' => '9e767f40b0a284a022376fe75662ea1b'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'rootMag',
+                'password' => 'magento',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '7fc_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '7fc_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'install' => [
+        'date' => 'Fri, 20 Sep 2019 13:56:56 +0000'
+    ]
+];
