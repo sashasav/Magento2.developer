26894b9979b3c3d89f7e73f0475571ce50aa2888
Oleksandr Savchuk <sashasavch@gmail.com>
2019-10-01 01:32:45 +0300

    modified owl-carousel

--- etc/env.php
+++ etc/env.php
@@ -10,7 +10,7 @@ return [
         'table_prefix' => '',
         'connection' => [
             'default' => [
-                'host' => 'localhost',
+                'host' => '127.0.0.1',
                 'dbname' => 'magento2',
                 'username' => 'rootMag',
                 'password' => 'magento',
